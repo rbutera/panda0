2064f27fcb0f361afed055176fa16704f2948ff0
Rai <rai@rbutera.com>
2016-10-03 14:39:12 +0100

    first try at integer limit

--- week1/integer-limit.c
+++ week1/integer-limit.c
@@ -1,9 +1,20 @@
 /* Find the limits of the int type */
 #include <stdio.h>
 
+int findMax(){
+	int max = 0;
+
+	while(max + 1 > max){
+		printf("%d\n", max);
+		max++;
+	}
+
+	return max;
+}
+
 int main() {
     setbuf(stdout, NULL);
-    int n = 42;
-    printf("%d\n", n);
+    int n = findMax();
+    printf("max is %d\n", n);
     return 0;
 }
