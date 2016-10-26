9c89a24a66e97cb93c538e3cf8b5826d96f7735a
Rai <rai@rbutera.com>
2016-10-26 17:48:50 +0100

    demonstrated adding of two vectors

---
+++ arrays/add-two-vectors.c
@@ -0,0 +1,30 @@
+/* Adds two vectors together, demonstrating how to deal with returning arrays from functions */
+
+/* For now, to design a function which adds two vectors,
+you can pass a third argument to hold the result, which
+works because of pass-by-reference */
+#include <stdio.h>
+
+#define VECTOR_DIMENSIONS 3
+
+void add(int n, int a[n], int b[n], int result[n]) {
+  for (int i = 0; i < n; i++) {
+    result[i] = a[i] + b[i];
+  }
+}
+
+int main (){
+  int first[VECTOR_DIMENSIONS] = {1,2,3};
+  int second[VECTOR_DIMENSIONS] = {2,4,6}; // expected sum would be {3,6,9};
+  int result[VECTOR_DIMENSIONS];
+
+  add(VECTOR_DIMENSIONS, first, second, result);
+
+  if (result[0] == 3 && result[1] == 6 && result[2] == 9) {
+    printf("added as expected");
+    return 0;
+  } else {
+    printf("didn't work");
+    return 1;
+  }
+}
