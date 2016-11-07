fae6f4c7215b8302262247a87604b64280499bda
Rai <rai@rbutera.com>
2016-11-07 17:56:30 +0000

    passed all 50 tests

--- week7/oxo.c
+++ week7/oxo.c
@@ -44,7 +44,7 @@ void newGame(Board *b, char player) {
 _Bool validRow (char c){
   _Bool result = (c == 'a' || c == 'b' || c == 'c');
   if(!result){
-    printf("invalid Row: %c\n", c);
+    // printf("invalid Row: %c\n", c);
   }
   return result;
 }
@@ -63,7 +63,7 @@ int convertRowCharToInt (char input) {
       break;
     default:
       output = -1;
-      printf("OMG INVALID ROW CHAR DETECTED!\n");
+      // printf("OMG INVALID ROW CHAR DETECTED!\n");
       break;
   }
   return output;
@@ -72,7 +72,7 @@ int convertRowCharToInt (char input) {
 _Bool validCol (char c){
   _Bool result = (c == '1' || c == '2' || c == '3');
   if(!result){
-    printf("invalid Col: %c\n", c);
+    // printf("invalid Col: %c\n", c);
   }
   return result;
 }
@@ -88,7 +88,7 @@ bool position(Board *b, char *text, Position *p) {
     _Bool valid = true;
 
     if(strlen(text) != 2) {
-      printf("string of length %i input - should be 2", (int) strlen(text));
+      // printf("position: length %i should be 2\n", (int) strlen(text));
       valid = false;
     } else if (!validRow(text[0]) || !validCol(text[1])) {
       valid = false;
@@ -109,6 +109,24 @@ bool position(Board *b, char *text, Position *p) {
     return valid;
 }
 
+char lastPlayer (Board *b){
+  char current = b->player;
+  char last;
+  switch(current){
+    case X:
+      last = O;
+      break;
+    case O:
+      last = X;
+      break;
+    default:
+      // printf("lastPlayer: fail, current = %c", current);
+      last = '?';
+      break;
+  }
+  return last;
+}
+
 char switchPlayer (Board *b){
   char current = b->player;
   char new;
@@ -120,10 +138,13 @@ char switchPlayer (Board *b){
       new = X;
       break;
     default:
-      printf("switchPlayer: failed to switch player, currently set to %c", current);
+      // printf("switchPlayer: failed to switch player, currently set to %c", current);
+      new = '?';
       break;
   }
-  b->player = new;
+  if(new != '?') {
+    b->player = new;
+  }
   return new;
 }
 
@@ -133,26 +154,96 @@ void move(Board *b, Position *p) {
   char player = b->player;
   int row = p->row;
   int col = p->col;
-  setAndDisplay(b, row, col, player);
-
+  setPosition(b, row, col, player);
+  switchPlayer(b);
+  b->moves++;
 }
 
 // TODO: finish this function
 // Check whether x, y, z form a winning line.
 bool line(char x, char y, char z) {
-    return false;
+    bool ans = x == y && y == z && x != B;
+    // printf("checking %c == %c == %c\n", x, y, z);
+    // printf("ans: ");
+    // printf(ans ? "true" : "false");
+    // printf("\n");
+    return ans;
+}
+
+bool checkHorizontalWins (Board *b){
+    int winningRow = -1;
+    bool winFound = false;
+    for (int row = 0; row < 3; row++) {
+      bool result = line(b->cells[row][0], b->cells[row][1], b->cells[row][2]);
+      if (result) {
+        winFound = true;
+        winningRow = row;
+        // printf("horizontal win found! row = %i", winningRow);
+      }
+    }
+    return winFound;
+}
+
+bool checkVerticalWins (Board *b){
+    int winningRow = -1;
+    bool winFound = false;
+    for (int row = 0; row < 3; row++) {
+      bool result = line(b->cells[0][row], b->cells[1][row], b->cells[2][row]);
+      if (result) {
+        winFound = true;
+        winningRow = row;
+        // printf("vertical win found! row = %i", winningRow);
+      }
+    }
+    return winFound;
+}
+
+bool checkDiagonalWins (Board *b){
+    int topLeft, topRight, centre, bottomLeft, bottomRight;
+    topLeft = b->cells[0][0];
+    topRight = b->cells[0][2];
+    bottomLeft = b->cells[2][0];
+    bottomRight = b->cells[2][2];
+    centre = b->cells[1][1];
+
+    if (line(topLeft,centre,bottomRight)) {
+      // printf("TL->BR diagonal win found\n");
+      return true;
+    } else if (line(topRight,centre,bottomLeft)) {
+      // printf("TR->BL diagonal win found\n");
+      return true;
+    } else {
+      // printf("no diagonal win found\n");
+      return false;
+    }
+}
+bool checkForWins (Board *b) {
+  bool hasWon = checkHorizontalWins(b) || checkVerticalWins(b) || checkDiagonalWins(b);
+  if (!hasWon){
+    // printf("no wins detected\n");
+  }
+  return hasWon;
 }
 
 // TODO: finish this function
 // Check whether or not the player who has just moved has won.
 bool won(Board *b) {
-    return false;
+    bool result = checkForWins(b);
+    // char player = lastPlayer(b);
+    if(result){
+      // printf("player %c has won!\n", player);
+    }
+    return result;
 }
 
 // TODO: finish this function
 // Check whether the game is drawn
 bool drawn(Board *b) {
-    return false;
+    bool boardNotWon = !checkForWins(b);
+    // printf("checking for draw\nboardNotWon: ");
+    // printf(boardNotWon ? "true" : "false");
+    // printf("\nb->moves == %i\n", b->moves);
+    return boardNotWon && (b->moves == 9);
 }
 
 // TODO: finish this function
@@ -195,12 +286,13 @@ Board *createBoard(char player) {
 int destroyBoard (Board * b) {
   free(b);
   // printf("destroyed board\n");
+  return 0;
 }
 
 int setPosition (Board *b, int row, int col, char newValue) {
     char currentValue = b->cells[row][col];
     if (currentValue == X || currentValue == O) {
-      printf("COULD NOT ASSIGN POSITION - TAKEN!\n");
+      // printf("COULD NOT ASSIGN POSITION - TAKEN!\n");
       return 1;
     } else {
       // printf("setting position %i %i to %c\n", row, col, newValue);
@@ -224,7 +316,20 @@ int setAndDisplay (Board *b, int row, int col, char newValue) {
     display(b);
     return 0;
   } else {
-    printf("could not set+display [%i,%i] to %c\n", row, col, newValue);
+    // printf("could not set+display [%i,%i] to %c\n", row, col, newValue);
+    return 1;
+  }
+}
+
+int getNextMove(Board *b, Position *p){
+  char player = X;
+  printf("\n Player %c turn: please type a position in the format \"xy\" (x = a,b,c) (y = 1,2,3)", player);
+  // TODO: get input
+  char *nextmove = "a1";
+  bool validMove = position(b, nextmove, p);
+  if (validMove) {
+    return 0;
+  } else {
     return 1;
   }
 }
@@ -232,11 +337,22 @@ int setAndDisplay (Board *b, int row, int col, char newValue) {
 // TODO: finish this function
 // Play the game interactively between two human players who take turns.
 void play(char player) {
-    Board *b = createBoard('X');
-    setAndDisplay(b, 1, 1, X);
-    setAndDisplay(b, 0, 1, O);
-    setAndDisplay(b, 0, 0, X);
-    setAndDisplay(b, 0, 1, O);
+    Position pdata = {-1,-1};
+    Position *pos = &pdata;
+
+    Board *b = createBoard(player);
+    printf("Player 1 is %c, Player 2 is not %c", player, player);
+    // ask
+    bool playing = true;
+
+    while(playing){
+      int nextMove = getNextMove(b,pos);
+      if (nextMove == 1) {
+        playing = false;
+      }
+    }
+
+    printf("thanks for playing!\n");
 }
 
 //-----------------------------------------------------------------------------
@@ -273,40 +389,40 @@ void test() {
 
     // Tests 1 to 5 (new board)
     newGame(board, X);
-    eqc(board->cells[0][0], B); // todo: TEST 1
-    eqc(board->cells[1][2], B); // todo: TEST 2
-    eqc(board->cells[2][1], B); // todo: TEST 3
-    eqc(board->player, X); // todo: TEST 4
-    eqc(board->moves, 0); // todo: TEST 5
+    eqc(board->cells[0][0], B);
+    eqc(board->cells[1][2], B);
+    eqc(board->cells[2][1], B);
+    eqc(board->player, X);
+    eqc(board->moves, 0);
 
     // Tests 6 to 14 (valid positions)
-    eqb(position(board, "a1", pos), true); // todo: TEST 6
-    eqi(pos->row, 0); // todo: TEST 7
-    eqi(pos->col, 0); // todo: TEST 8
-    eqb(position(board, "b3", pos), true); // todo: TEST 9
-    eqi(pos->row, 1); // todo: TEST 10
-    eqi(pos->col, 2); // todo: TEST 11
-    eqb(position(board, "c1", pos), true); // todo: TEST 12
-    eqi(pos->row, 2); // todo: TEST 13
-    eqi(pos->col, 0); // todo: TEST 14
+    eqb(position(board, "a1", pos), true);
+    eqi(pos->row, 0);
+    eqi(pos->col, 0);
+    eqb(position(board, "b3", pos), true);
+    eqi(pos->row, 1);
+    eqi(pos->col, 2);
+    eqb(position(board, "c1", pos), true);
+    eqi(pos->row, 2);
+    eqi(pos->col, 0);
 
     // Tests 15 to 22 (invalid positions, and occupied squares)
-    eqb(position(board, "d2", pos), false); // todo: TEST 15
-    eqb(position(board, "b0", pos), false); // todo: TEST 16
-    eqb(position(board, "b4", pos), false); // todo: TEST 17
-    eqb(position(board, "2b", pos), false); // todo: TEST 18
-    eqb(position(board, "b2x", pos), false); // todo: TEST 19
-    eqb(position(board, "b", pos), false); // todo: TEST 20
-    eqb(position(board, "", pos), false); // todo: TEST 21
+    eqb(position(board, "d2", pos), false);
+    eqb(position(board, "b0", pos), false);
+    eqb(position(board, "b4", pos), false);
+    eqb(position(board, "2b", pos), false);
+    eqb(position(board, "b2x", pos), false);
+    eqb(position(board, "b", pos), false);
+    eqb(position(board, "", pos), false);
     *board = (Board) {{{B,B,B},{B,B,B},{B,X,B}},O,1};
-    eqb(position(board, "c2", pos), false); // todo: TEST 22
+    eqb(position(board, "c2", pos), false);
 
     // Tests 23 to 28 (moves)
     newGame(board, 'X');
     position(board, "b2", pos);
     move(board, pos);
-    eqc(board->cells[1][1], X); // todo: TEST 23
-    eqc(board->player, O); // todo: TEST 24
+    eqc(board->cells[1][1], X);
+    eqc(board->player, O);
     eqc(board->moves, 1); // todo: TEST 25
     position(board, "a3", pos);
     move(board, pos);
