519bfc49bb32b785bf27056eac17fbf917edfdde
Rai <rai@rbutera.com>
2016-11-27 20:32:12 +0000

    driving me nuts

--- week9/sketch.c
+++ week9/sketch.c
@@ -2,20 +2,168 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "display.h"
+#include <string.h> // TODO: check if this is OK
 
 // TODO: upgrade the run function, adding functions to support it.
+
+// constants representing possible opcodes (the first two bits of an instruction byte)
+const int DX = 0;
+const int DY = 1;
+const int DT = 2;
+const int PEN = 3;
+
+// storing bytes as unsigned chars
+typedef unsigned char Byte;
+
+// TODO: try fix enumerated type representing an opcode
+// typedef enum {DX, DY, DT, PEN} OpCode;
+
+// .sketch file instruction byte
+typedef struct InstructionByte InstructionByte;
+struct InstructionByte {
+  Byte raw; // store a copy of the uninterpreted raw instruction
+  // OpCode opcode; // instruction opcode (first 2 bits)
+  int opcode; // instruction opcode (first 2 bits) TODO: change to enumerated type
+  signed int operand; // instruction operand (last 6 bits)
+};
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
+
+int printBitsNL (Byte wtf){
+  printBits(1, &wtf);
+  // printf("\n");
+  return 0;
+}
+
+int testWTF(Byte wtf){
+  printBitsNL(wtf);
+  wtf = wtf<<2;
+  printBitsNL(wtf);
+  wtf = ~wtf;
+  printBitsNL(wtf);
+  wtf += 1;
+  printBitsNL(wtf);
+  wtf = wtf>>2;
+  printf("%d\n", wtf);
+  printBitsNL(wtf);
+  return 0;
+}
+
+_Bool operandIsPositive (Byte wtf){
+  // printf("is operand of %d positive or negative?\nbinary: ", wtf);
+  // printBitsNL(wtf);
+  // printf("shift2L\nbinary: ");
+  wtf = wtf<<2;
+  // printBitsNL(wtf);
+  // printf("shift2R\nbinary: ");
+  wtf = wtf>>2;
+  // printBitsNL(wtf);
+  // printf("result: %i\n", wtf);
+  if (wtf>>2 & 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+char positiveOrNegative (Byte x){
+  return (operandIsPositive(x) ? 'p' : 'n');
+}
+
+signed int extractOperand (Byte input){
+  _Bool needsFlipping = !operandIsPositive(input);
+  if (needsFlipping) {
+    Byte masked = input & 0x3f;
+    return ~masked + 1;
+  } else {
+    return input & 0x3f;
+  }
+}
+
+int extractOpcode (Byte input){
+  Byte masked = input & 0xC0;
+  return masked >> 6;
+}
+
+/**
+ * read bytes from (already opened) in file, unpack each byte into an opcode and operand, and use those to make suitable calls to display functions
+ * @param  *in file to read
+ * @param  d  display to call functions to
+ * @return    [description]
+ */
+int interpret(FILE *in, display *d) {
+  /* code */
+  return 0;
+}
+
+void printPosNeg (Byte testing){
+  printf("0x%02hhx is %c\n", testing, positiveOrNegative(testing));
+}
+
+char *stringifyOpcode (int code, char *destination) {
+  char *result;
+
+  switch (code) {
+    case 0:
+      result = "DX";
+    case 1:
+      result = "DY";
+    case 2:
+      result = "DT";
+    case 3:
+      result = "PEN";
+    default:
+      result = "ERROR";
+  }
+
+  strcpy(destination, result);
+  return result;
+}
 
 // Read sketch instructions from the given file.  If test is NULL, display the
 // result in a graphics window, else check the graphics calls made.
 void run(char *filename, char *test[]) {
-    FILE *in = fopen(filename, "rb");
-    if (in == NULL) {
-        fprintf(stderr, "Can't open %s\n", filename);
-        exit(1);
-    }
-    display *d = newDisplay(filename, 1024, 1024, test);
-    end(d);
-    fclose(in);
+    Byte example = 0x7D;
+
+    InstructionByte exampleInstruction = {
+      .raw = example,
+      .opcode = extractOpcode(example),
+      .operand = extractOperand(example)
+    };
+
+    char parsedOpcode[4];
+    stringifyOpcode(exampleInstruction.opcode, parsedOpcode);
+
+    printf("the example instruction (0x%02hhx) translates to %s %i.\n", example, parsedOpcode, exampleInstruction.operand);
+    printf("binary:");
+    printBitsNL(exampleInstruction.raw);
+
+    // printf("lol examples of signed ints: [%i,%i]", minusOne, minusNine);
+    // printf("")
+
+    // FILE *in = fopen(filename, "rb");
+    // if (in == NULL) {
+    //     fprintf(stderr, "Can't open %s\n", filename);
+    //     exit(1);
+    // }
+    // display *d = newDisplay(filename, 1024, 1024, test);
+    // end(d);
+    // fclose(in);
 }
 
 // ----------------------------------------------------------------------------
