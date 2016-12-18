83cf895953483b76bec7943a21b4e1f0cd7dadb9
Rai <rai@rbutera.com>
2016-12-18 17:31:30 +0000

    skeleton tests for rest of module

--- week12/test.c
+++ week12/test.c
@@ -37,16 +37,17 @@ static char *test_newNode()
     list *nodeless = newList(99);
 
     char *aData = malloc((sizeof(char) * 3) + 1);
+    char *bData = malloc((sizeof(char) * 3) + 1);
+    char *cData = malloc((sizeof(char) * 10) + 8);
 
     aData = "foo";
-    char *bData = malloc((sizeof(char) * 3) + 1);
     bData = "bar";
-    char *cData = malloc((sizeof(char) * 10) + 8);
     cData = "Raimondo";
 
     node *a = newNode(aData, nodeless->start, nodeless->end);
     node *b = newNode(bData, a, nodeless->end);
     node *c = newNode(cData, b, nodeless->end);
+
     a->next = b;
     b->next = c;
 
@@ -75,12 +76,124 @@ static char *test_removeList()
 }
 
 
+static char *test_start()
+{
+    mu_assert("start implemented", false);
+    return 0;
+}
+
+
+static char *test_end()
+{
+    mu_assert("end implemented", false);
+    return 0;
+}
+
+
+static char *test_atStart()
+{
+    mu_assert("atStart implemented", false);
+    return 0;
+}
+
+
+static char *test_atEnd()
+{
+    mu_assert("atEnd implemented", false);
+    return 0;
+}
+
+
+static char *test_forward()
+{
+    mu_assert("forward implemented", false);
+    return 0;
+}
+
+
+static char *test_backward()
+{
+    mu_assert("backward implemented", false);
+    return 0;
+}
+
+
+static char *test_insertBefore()
+{
+    mu_assert("insertBefore implemented", false);
+    return 0;
+}
+
+
+static char *test_insertAfter()
+{
+    mu_assert("insertAfter implemented", false);
+    return 0;
+}
+
+
+static char *test_getBefore()
+{
+    mu_assert("getBefore implemented", false);
+    return 0;
+}
+
+
+static char *test_getAfter()
+{
+    mu_assert("getAfter implemented", false);
+    return 0;
+}
+
+
+static char *test_setBefore()
+{
+    mu_assert("setBefore implemented", false);
+    return 0;
+}
+
+
+static char *test_setAfter()
+{
+    mu_assert("setAfter implemented", false);
+    return 0;
+}
+
+
+static char *test_deleteBefore()
+{
+    mu_assert("deleteBefore implemented", false);
+    return 0;
+}
+
+
+static char *test_deleteAfter()
+{
+    mu_assert("deleteAfter implemented", false);
+    return 0;
+}
+
+
 static char *all_tests()
 {
     mu_run_test(test_check_testing_works);
     mu_run_test(test_newList);
     mu_run_test(test_removeList);
     mu_run_test(test_newNode);
+    mu_run_test(test_start);
+    mu_run_test(test_end);
+    mu_run_test(test_atStart);
+    mu_run_test(test_atEnd);
+    mu_run_test(test_forward);
+    mu_run_test(test_backward);
+    mu_run_test(test_insertBefore);
+    mu_run_test(test_insertAfter);
+    mu_run_test(test_getBefore);
+    mu_run_test(test_getAfter);
+    mu_run_test(test_setBefore);
+    mu_run_test(test_setAfter);
+    mu_run_test(test_deleteBefore);
+    mu_run_test(test_deleteAfter);
     return 0;
 }
 
